42d529b9d562e726c6c7fdf35660c8064d8850ca
marychka2335 <marychka2335@icloud.com>
2024-02-29 20:11:47 +0200

    hw commit

---
+++ src/components/App.jsx
@@ -0,0 +1,59 @@
+import { Component } from 'react';
+import Notiflix from 'notiflix';
+import css from './App.module.css';
+import { PhonebookForm } from './PhonebookForm/PhonebookForm';
+import { ContactsList } from './ContactsList/ContactsList';
+import { Filter } from './Filter/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  handleAddContact = contact => {
+    if (this.state.contacts.some(item => item.name === contact.name)) {
+      Notiflix.Notify.failure(`${contact.name} is already in contacts`);
+      return true;
+    }
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, contact],
+      };
+    });
+    return false;
+  };
+
+  handleDeleteContact = id => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(contact => contact.id !== id),
+      };
+    });
+  };
+
+  handleChangeFilter = evt => {
+    this.setState({ filter: evt.target.value });
+  };
+
+  handleFilterContacts = () => {
+    return this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(this.state.filter.toLowerCase().trim())
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <h1 className={css.titleForm}>Phonebook</h1>
+        <PhonebookForm addContact={this.handleAddContact} />
+        <h2 className={css.titleContacts}>Contacts</h2>
+        <Filter value={this.state.filter} handleChange={this.handleChangeFilter} />
+        <ContactsList
+          contacts={this.handleFilterContacts()}
+          deleteContact={this.handleDeleteContact}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,21 @@
+// import React from 'react';
+import PropTypes from 'prop-types';
+import css from './Filter.module.css';
+
+export function Filter({ handleChange, value }) {
+  return (
+    <input
+      className={css.inputSearch}
+      type="text"
+      value={value}
+      onChange={handleChange}
+      name="filter"
+      placeholder="Search"
+    />
+  );
+}
+
+Filter.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
